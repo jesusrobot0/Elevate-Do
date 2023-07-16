143fc1eb1aee6a435ce9eede6e534ff13fbbd52c
jesusrobot0 <jesusrobot9000@icloud.com>
2023-07-16 14:30:30 -0500

    Agregada la funcionalidad de agregar todos al state princial desde el Form

--- src/ElevateDo.jsx
+++ src/ElevateDo.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import { Navbar } from './components/nav-bar/Navbar'
 import { Form } from './components/form/Form'
 import { TodoList } from './components/todo-list/TodoList'
 import styles from './styles/eletavete-do.module.css'
 
 export function ElevateDo() {
+  const [todos, setTodos] = useState([])
+
+  const handleAddTodo = (newTodo) => {
+    setTodos([...todos, newTodo])
+  }
+
   return (
     <>
       <header className={`wrapper ${styles.app__header}`}>
@@ -11,7 +18,7 @@ export function ElevateDo() {
       </header>
       <div className={`wrapper ${styles.app__body}`}>
         <aside className={styles.app__sidebar}>
-          <Form />
+          <Form onNewTodo={handleAddTodo} />
         </aside>
         <main className={styles.app__main}>
           <TodoList />
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,8 +1,38 @@
+import { useState } from 'react'
+import { nanoid } from 'nanoid'
+import PropTypes from 'prop-types'
 import styles from './form.module.css'
 
-export function Form() {
+export function Form({ onNewTodo }) {
+  const initialTodo = {
+    id: '',
+    date: '',
+    title: '',
+    description: '',
+    status: false,
+  }
+
+  const [todo, setTodo] = useState(initialTodo)
+
+  const handleChange = (event) => {
+    setTodo({
+      ...todo,
+      [event.target.id]: event.target.value,
+    })
+  }
+
+  const handleSubtmit = (event) => {
+    event.preventDefault()
+
+    todo.id = nanoid()
+    todo.date = new Date()
+
+    onNewTodo(todo)
+    setTodo(initialTodo)
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubtmit}>
       <div className={styles.form__field}>
         <label htmlFor="title" className={styles.form__label}>
           Title
@@ -12,6 +42,8 @@ export function Form() {
           id="title"
           placeholder="E.g. Total world domination"
           className={styles.form__input}
+          value={todo.title}
+          onChange={handleChange}
         />
       </div>
       <div className={styles.form__field}>
@@ -22,9 +54,15 @@ export function Form() {
           id="description"
           placeholder="E. g. tidy my room, then conquer latam"
           className={styles.form__area}
+          value={todo.description}
+          onChange={handleChange}
         ></textarea>
       </div>
       <input type="submit" value="Save" className={styles.form__button} />
     </form>
   )
+}
+
+Form.propTypes = {
+  onNewTodo: PropTypes.func.isRequired,
 }
