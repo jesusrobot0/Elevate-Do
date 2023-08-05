8098d558b97691a845b1782f1e370cfbec0128f3
jesusrobot0 <jesusrobot9000@icloud.com>
2023-08-05 18:07:47 -0500

    Optimizing the export of the /components barrel file

--- src/components/index.js
+++ src/components/index.js
@@ -1,21 +1,9 @@
-import { Contact } from './contact/Contact.jsx'
-import { ErrorForm } from './error-form/ErrorForm.jsx'
-import { Form } from './form/Form.jsx'
-import { Navbar } from './nav-bar/Navbar.jsx'
-import { Placeholder } from './placeholder/Placeholder.jsx'
-import { SearchBar } from './search-bar/SearchBar.jsx'
-import { TodoItem } from './todo-item/TodoItem.jsx'
-import { TodoList } from './todo-list/TodoList.jsx'
-import { TodoSummary } from './todos-summary/TodosSummary.jsx'
-
-export {
-  Contact,
-  ErrorForm,
-  Form,
-  Navbar,
-  Placeholder,
-  SearchBar,
-  TodoItem,
-  TodoList,
-  TodoSummary,
-}
+export { Contact } from './contact/Contact.jsx'
+export { ErrorForm } from './error-form/ErrorForm.jsx'
+export { Form } from './form/Form.jsx'
+export { Navbar } from './nav-bar/Navbar.jsx'
+export { Placeholder } from './placeholder/Placeholder.jsx'
+export { SearchBar } from './search-bar/SearchBar.jsx'
+export { TodoItem } from './todo-item/TodoItem.jsx'
+export { TodoList } from './todo-list/TodoList.jsx'
+export { TodoSummary } from './todos-summary/TodosSummary.jsx'
